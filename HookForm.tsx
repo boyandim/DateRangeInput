e34800dfd13dca3948e1e4121b0f79b18bcda121
Boyan Dimitrov <boyan85@gmail.com>
2022-04-19 09:10:41 +0300

    Initial commit

---
+++ HookForm.tsx
@@ -0,0 +1,84 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Stack
+} from "@chakra-ui/react";
+import { DevTool } from "@hookform/devtools";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as React from "react";
+import "react-app-polyfill/ie11";
+import { FormProvider as HookFormProvider, useForm } from "react-hook-form";
+import validator from "validator";
+import * as z from "zod";
+import { DateRangeInput, DateSingleInput } from "./sr";
+
+const dateValidation = z
+  .string()
+  .length(String("MM/dd/yyyy").length, "Date is required")
+  .refine(
+    (date) =>
+      validator.isDate(date, { format: "MM/dd/yyyy", strictMode: true }),
+    {
+      message: "Date is required and must be in the form of mm/dd/yyyy"
+    }
+  );
+
+const schema = z.object({
+  someDate: dateValidation,
+  startDate: dateValidation,
+  endDate: dateValidation
+});
+
+type Schema = z.infer<typeof schema>;
+
+export function HookForm() {
+  function onSubmit(values: Schema) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log(JSON.stringify(values, null, 2));
+        resolve(true);
+      }, 500);
+    });
+  }
+
+  const methods = useForm({
+    resolver: zodResolver(schema)
+  });
+
+  const { handleSubmit, errors, register, control, formState } = methods;
+
+  return (
+    <HookFormProvider {...methods}>
+      <DevTool control={control} />
+      <Box as="form" onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={5} my={5}>
+          <FormControl isInvalid={!!errors?.someDate}>
+            <FormLabel htmlFor="someDate">Some Date</FormLabel>
+            <DateSingleInput ref={register} id="someDate" name="someDate" />
+            <FormErrorMessage>{errors?.someDate?.message}</FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={!!errors?.startDate || !!errors?.endDate}>
+            <FormLabel>Date Range</FormLabel>
+            <DateRangeInput startRef={register} endRef={register} />
+            <FormHelperText>Helper text goes here</FormHelperText>
+            <FormErrorMessage>
+              {errors?.startDate?.message || errors?.endDate?.message}
+            </FormErrorMessage>
+          </FormControl>
+        </Stack>
+        <Button
+          type="submit"
+          colorScheme="teal"
+          isLoading={formState.isSubmitting}
+        >
+          Submit
+        </Button>
+      </Box>
+    </HookFormProvider>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import {
+  ChakraProvider,
+  Container,
+  extendTheme,
+  Heading
+} from "@chakra-ui/react";
+import * as React from "react";
+import "react-app-polyfill/ie11";
+import * as ReactDOM from "react-dom";
+import { HookForm } from "./HookForm";
+
+const theme = extendTheme({});
+
+const App = () => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Container mt={10}>
+        <Heading>React Chakra-UI Date Picker</Heading>
+        <HookForm />
+      </Container>
+    </ChakraProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
